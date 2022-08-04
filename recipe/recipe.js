d5a9652045adf3c6040e1030d4240acd204a8537
Ruth Irvin <ruth.diane.irvin@gmail.com>
2022-08-04 15:05:03 -0500

    adds dom manipulation button

---
+++ recipe/recipe.js
@@ -0,0 +1,15 @@
+const container = document.getElementById('moreRecipes');
+const btn = document.createElement('button');
+btn.textContent = 'Print this Most Perfect Recipe';
+btn.addEventListener("click", function() {
+  print();
+});
+container.appendChild(btn);
+
+
+
+
+
+//<div id="more recipes">
+//resource used for syntax : https://stackoverflow.com/questions/49864972/manipulate-dom-with-js-add-button-link
+//<script type="text/javascript" src="recipe.js"></script>
