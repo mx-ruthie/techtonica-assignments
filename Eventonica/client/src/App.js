399b5facba8068852337e1032fb61b2c7f2f46ea
Ruth Irvin <ruth.diane.irvin@gmail.com>
2022-09-19 12:35:58 -0500

    creates footer component

--- Eventonica/client/src/App.js
+++ Eventonica/client/src/App.js
@@ -1,5 +1,6 @@
 import calendar from "./calendar.png";
 import "./App.css";
+import Footer from "./components/footer";
 
 function App() {
   return (
@@ -98,19 +99,8 @@ function App() {
           </div>
         </aside>
       </main>
+<Footer />
 
-      <footer>
-        <div>
-          Star Calendar favicon made by
-          <a href="https://www.flaticon.com/authors/freepik" title="Freepik">
-            Freepik
-          </a>
-          Find your own on
-          <a href="https://www.flaticon.com/" title="Flaticon">
-            FlatIcon.com
-          </a>
-        </div>
-      </footer>
     </div>
   );
 }
